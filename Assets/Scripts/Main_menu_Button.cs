b2825a23f08c0f6d959c8dc645cc0deb39d62af2
Rowan Visser2 <rowan.visser2@hva.nl>
2017-02-21 11:34:23 +0100

    User interface
    User interface

---
+++ Assets/Scripts/Main_menu_Button.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_menu_Button : MonoBehaviour {
+
+	// Use this for initialization
+	public void ScanClick () {
+        SceneManager.LoadScene("Vuforia_Test");
+    }
+
+
+}
